c6ae67f5c6dcd3aec640545f900f5191b0c61146
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-15 09:02:25 +0530

    Update Sensor.h

--- Sensor.h
+++ Sensor.h
@@ -12,8 +12,8 @@ typedef struct{
   int id;
 }SensorType;
 
-SensorType SensorConfig[MAX_SENSOR_SUPPORTED];
-int SendSensorReading(int maxSamples, int maxSensorsupported, void (*printIO)(int, int), FILE *fp);
-int GetandPrintSensorReading(SensorType sensorConfig, int maxSamples, void (*printIO)(int, int), FILE *fp);
+extern SensorType SensorConfig[MAX_SENSOR_SUPPORTED];
+int SendSensorReading(int maxSamples, int maxSensorsupported, void (*printIO)(int, int, FILE*), FILE *fp);
+int GetandPrintSensorReading(SensorType sensorConfig, int maxSamples, void (*printIO)(int, int, FILE*), FILE *fp);
 
 #endif
