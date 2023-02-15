20cf448c00f754a3dd51268aeb1870636f88565c
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-15 10:49:53 +0530

    Update TestSender.c

--- TestSender.c
+++ TestSender.c
@@ -14,12 +14,12 @@ int TestSenderFormat(int maxSamples, FILE* fp)
   int socCnt = 0;
   regex_t compPattern;  
   //Matching pattern
-  const char* pattern = "abc"; 
+  const char* pattern = "[1-9]+"; 
   if (regcomp(&compPattern, pattern, REG_EXTENDED|REG_NOSUB) != 0) return 0; 
   char *string = (char*)calloc(50, sizeof(int));  
   while (!feof(fp)) {
      if( fgets (string, 50, fp)!=NULL ) {
-          int status = regexec(&compPattern, 123, 0, NULL, 0);
+          int status = regexec(&compPattern, "123", 0, NULL, 0);
           assert(status == 0); 
           if(strstr(string, "Temperature")!= NULL){
                tempCnt ++;
