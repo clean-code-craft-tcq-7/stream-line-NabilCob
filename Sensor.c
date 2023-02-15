4a46cd80bc0ce02a2f1c07e8d28ba23d1d7e34c2
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-15 09:05:00 +0530

    Update Sensor.c

--- Sensor.c
+++ Sensor.c
@@ -8,19 +8,22 @@ SensorType SensorConfig[MAX_SENSOR_SUPPORTED] =
   {SOC, 2}
 };
 
-int SendSensorReading(int maxSamples, int maxSensorsupported, void (*printIO)(int, int), FILE *fp)
+int SendSensorReading(int maxSamples, int maxSensorsupported, void (*printIO)(int, int, FILE*), FILE *fp)
 {
      int lstIdx;
+     int retVal = 0;
      for(lstIdx = 0; lstIdx < maxSensorsupported; lstIdx++){
-         GetandPrintSensorReading(SensorConfig[lstIdx], maxSamples, printIO, fp);
+         retVal = GetandPrintSensorReading(SensorConfig[lstIdx], maxSamples, printIO, fp);
      }
+     return retVal; 
 }
 
-int GetandPrintSensorReading(SensorType sensorConfig, int maxSamples, void (*printIO)(int, int), FILE *fp)
+int GetandPrintSensorReading(SensorType sensorConfig, int maxSamples, void (*printIO)(int, int, FILE*), FILE *fp)
 {
     srand(sensorConfig.id);
     int smpCnt;
     for(smpCnt = 0; smpCnt < maxSamples; smpCnt ++){
           (*printIO)(sensorConfig.name, rand(), fp);
     }
+    return smpCnt;
 }
