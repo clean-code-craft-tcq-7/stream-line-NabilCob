7ddfe2a10267c3276b80c1c3ef0bc65bceac4511
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-15 09:37:32 +0530

    Create TestSender.c

---
+++ test/TestSender.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <regex.h>
+#include <string.h>
+
+#include "Sensor.h"
+#include "PrintIO.h"
+#include "TestSender.h"
+
+void TestSenderFormat(int maxSamples)
+{
+  int tempCnt = 0;
+  int socCnt = 0;
+  regex_t compPattern;  
+  //Matching pattern
+  const char* pattern = "[a-zA-Z]+=\d+\n";  
+  //Verify pattern
+  if (regcomp(&compPattern, pattern, REG_EXTENDED) != 0) return 0; 
+  char *string = (char*)calloc(50, sizeof(int));  
+  while (!feof(fp)) {
+     if( fgets (string, 50, fp)!=NULL ) {
+          int status = regexec(&compPattern, string, 0, NULL, 0);
+          assert(status == 1); 
+          if(strstr(string, "Temperature")!= NULL){
+               tempCnt ++;
+           }
+          else if(strstr(string, "SOC")!= NULL){
+               socCnt ++;
+          }
+          else{
+              //Do nothing
+          }         
+     }
+  }
+  //verify number of samples read
+  assert(tempCnt == maxSamples);
+  assert(socCnt = maxSamples); 
+  regfree(&compPattern); 
+}
