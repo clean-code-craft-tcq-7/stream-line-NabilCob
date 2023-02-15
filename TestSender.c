d335774398c93fdf7b7a343c7f016404264c103c
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-15 10:03:44 +0530

    Update TestSender.c

--- TestSender.c
+++ TestSender.c
@@ -14,7 +14,7 @@ int TestSenderFormat(int maxSamples, FILE* fp)
   int socCnt = 0;
   regex_t compPattern;  
   //Matching pattern
-  const char* pattern = "[a-zA-Z]+=\d+\n";  
+  const char* pattern = "[a-zA-Z]+\s=\d+\n"; 
   //Verify pattern
   if (regcomp(&compPattern, pattern, REG_EXTENDED) != 0) return 0; 
   char *string = (char*)calloc(50, sizeof(int));  
