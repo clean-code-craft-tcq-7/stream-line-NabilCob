330aed96f881d5eef6071e9ee201978632562ed7
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-15 09:37:20 +0530

    Update Sender.c

--- Sender.c
+++ Sender.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <assert.h>
-#include <regex.h>
-#include <string.h>
 
 #include "Sensor.h"
 #include "PrintIO.h"
+#include "TestSender.h"
 
 int main(){
   
@@ -13,8 +11,6 @@ int main(){
   FILE *fp;
   int maxSamples = 50;
   int maxSensorsupported = 2;
-  int tempCnt = 0;
-  int socCnt = 0;
   int retStatus = 0;
   fp = fopen("sensoroutput.txt", "w+");
   if(fp == NULL){
@@ -22,34 +18,11 @@ int main(){
      return 0;
   }
   retStatus = SendSensorReading(maxSamples, maxSensorsupported, FormatOutput, fp);  
-  
-  //Test
-  rewind(fp);
   assert(retStatus == maxSamples); 
-  regex_t compPattern;  
-  const char* pattern = "[a-zA-Z]+=\d+\n";  
-  if (regcomp(&compPattern, pattern, REG_EXTENDED) != 0) return 0; 
-  char *string = (char*)calloc(50, sizeof(int));  
-  while (!feof(fp)) {
-     if( fgets (string, 50, fp)!=NULL ) {
-          int status = regexec(&compPattern, string, 0, NULL, 0);
-          assert(status == 1); 
-          if(strstr(string, "Temperature")!= NULL){
-               tempCnt ++;
-           }
-          else if(strstr(string, "SOC")!= NULL){
-               socCnt ++;
-          }
-          else{
-              //Do nothing
-          }         
-     }
-  }
-  printf("\n %d, %d", tempCnt, socCnt);
-  assert(tempCnt == maxSamples);
-  assert(socCnt = maxSamples); 
-  regfree(&compPattern); 
-
+  
+  //Test Sender Format
+  rewind(fp);
+  TestSenderFormat(maxSamples,fp);
   fclose(fp);
   return 0;
 }
